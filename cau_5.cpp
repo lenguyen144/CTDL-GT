096159c64622bb51fdb1f60ad88d76211ad08ac1
yunttan <xamykieu@gmail.com>
2026-04-10 14:11:15 +0700

    tich hop merge sort

--- cau_5.cpp
+++ cau_5.cpp
@@ -21,7 +21,49 @@ struct node {
 	node* next;
 	node(sv x) : data(x), next(nullptr) {}
 };
+class MergeSortList {
+public:
+    static node* merge(node* a, node* b) {
+        if (!a) return b;
+        if (!b) return a;
 
+        if (a->data.MSSV <= b->data.MSSV) {
+            a->next = merge(a->next, b);
+            return a;
+        } else {
+            b->next = merge(a, b->next);
+            return b;
+        }
+    }
+
+    static node* getMiddle(node* head) {
+        if (!head) return head;
+
+        node* slow = head;
+        node* fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    static node* mergeSort(node* head) {
+        if (!head || !head->next)
+            return head;
+
+        node* mid = getMiddle(head);
+        node* half = mid->next;
+        mid->next = nullptr;
+
+        node* left = mergeSort(head);
+        node* right = mergeSort(half);
+
+        return merge(left, right);
+    }
+};
 
 class Circular_Linked_List {
 private:
@@ -50,7 +92,7 @@ public:
 		}
 	}
 
-	void print();
+
 
 	void add_to_last(sv data) {
 		if (this->last == nullptr) {
@@ -67,13 +109,57 @@ public:
 		last = p;
 
 	}
+	 void print() {
+        if (!last) return;
 
+        node* p = last->next;
+        do {
+            std::cout << p->data.ho_ten << " - "
+                 << p->data.MSSV << " - "
+                 << p->data.nam_sinh << " - "
+                 << p->data.lop << std::endl;
+            p = p->next;
+        } while (p != last->next);
+    }
+
+
+    // ham sap xep merge sort
+    void sort_MSSV() {
+        if (!last || last->next == last) return;
+
+        // cat vong
+        node* head = last->next;
+        last->next = nullptr;
+
+        // goi merge sort
+        head = MergeSortList::mergeSort(head);
+
+        // noi lai vong
+        node* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+
+        temp->next = head;
+        last = temp;
+    }
 };
-
-
-
 int main() {
+    Circular_Linked_List ds;
 
+    ds.add_to_last(sv("Nguyen Hoa Hau", 20093, 2002, "D20"));
+    ds.add_to_last(sv("Do Luc Si", 20079, 2000, "D20"));
+    ds.add_to_last(sv("Ly Minh Tinh", 21085, 2003, "D21"));
+    ds.add_to_last(sv("Tran Dai Gia", 21096, 2003, "D21"));
+    ds.add_to_last(sv("Duong Vo Su", 18011, 2000, "D18"));
+
+    std::cout << "Truoc khi sap xep:\n";
+    ds.print();
+
+    ds.sort_MSSV();
+
+    std::cout << "\nSau khi sap xep:\n";
+    ds.print();
 
 	return 0;
 }
