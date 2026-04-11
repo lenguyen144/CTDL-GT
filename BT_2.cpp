3c0dbe6d513383e9c904a6c57b6399012f7e72e7
yunttan <xamykieu@gmail.com>
2026-04-11 11:10:14 +0700

    them add to last

--- BT_2.cpp
+++ BT_2.cpp
@@ -56,10 +56,32 @@ public:
     }
 
     // ham in danh sach sinh vien
-    void print();
+    void print() {
+        node* temp = first;
+        while (temp != nullptr) {
+            std::cout << temp->data.ho_ten << "  "
+                      << temp->data.MSSV << "  "
+                      << temp->data.nam_sinh << "  "
+                      << temp->data.lop << std::endl;
+            temp = temp->next;
+        }
+    }
 
     // ham them 1 sinh vien vao cuoi danh sach
-    void add_to_last(sv data);
+    void add_to_last(sv data) {
+        node* p = new node(data);
+
+        if (first == nullptr) {
+            first = p;
+            return;
+        }
+
+        node* temp = first;
+        while (temp->next != nullptr)
+            temp = temp->next;
+
+        temp->next = p;
+    }
 };
 
 // DANH SACH LIEN KET VONG
@@ -94,10 +116,33 @@ public:
     }
 
     // ham in danh sach vong
-    void print();
+    void print() {
+        if (last == nullptr) return;
+
+        node* temp = last->next; // node dau
+        do {
+            std::cout << temp->data.ho_ten << "  "
+                      << temp->data.MSSV << "  "
+                      << temp->data.nam_sinh << "  "
+                      << temp->data.lop << std::endl;
+            temp = temp->next;
+        } while (temp != last->next);
+    }
 
     // ham them node vao cuoi danh sach vong
-    void add_to_last(sv data);
+    void add_to_last(sv data) {
+        node* p = new node(data);
+
+        if (last == nullptr) {
+            last = p;
+            last->next = last;
+            return;
+        }
+
+        p->next = last->next; // tro ve node dau
+        last->next = p;
+        last = p;
+    }
 };
 
 // NODE DANH SACH DOI
@@ -137,14 +182,77 @@ public:
     }
 
     // ham in danh sach
-    void print();
+    void print() {
+        node_d* temp = first;
+        while (temp != nullptr) {
+            std::cout << temp->data.ho_ten << "  "
+                      << temp->data.MSSV << "  "
+                      << temp->data.nam_sinh << "  "
+                      << temp->data.lop << std::endl;
+            temp = temp->right;
+        }
+    }
 
     // ham them vao cuoi danh sach doi
-    void add_to_last(sv data);
+    void add_to_last(sv data) {
+        node_d* p = new node_d(data);
+
+        if (first == nullptr) {
+            first = p;
+            return;
+        }
+
+        node_d* temp = first;
+        while (temp->right != nullptr)
+            temp = temp->right;
+
+        temp->right = p;
+        p->left = temp;
+    }
 };
 
 int main() {
-	std::cout << "tai la hoc tro cung thay thinh" << std::endl;
 
-	return 0;
+    // tao 3 danh sach
+    Singly_Linked_List ds_don;
+    Circular_Linked_List ds_vong;
+    Doubly_Linked_List ds_kep;
+
+    // du lieu theo de
+    sv sv1("Nguyen Hoa Hau", "20093", 2002, "D20");
+    sv sv2("Do Luc Si", "20079", 2000, "D20");
+    sv sv3("Ly Minh Tinh", "21085", 2003, "D21");
+    sv sv4("Tran Dai Gia", "21096", 2003, "D21");
+    sv sv5("Duong Vo Su", "18011", 2000, "D18");
+
+    // them vao cuoi
+    ds_don.add_to_last(sv1);
+    ds_don.add_to_last(sv2);
+    ds_don.add_to_last(sv3);
+    ds_don.add_to_last(sv4);
+    ds_don.add_to_last(sv5);
+
+    ds_vong.add_to_last(sv1);
+    ds_vong.add_to_last(sv2);
+    ds_vong.add_to_last(sv3);
+    ds_vong.add_to_last(sv4);
+    ds_vong.add_to_last(sv5);
+
+    ds_kep.add_to_last(sv1);
+    ds_kep.add_to_last(sv2);
+    ds_kep.add_to_last(sv3);
+    ds_kep.add_to_last(sv4);
+    ds_kep.add_to_last(sv5);
+
+    // in ra
+    std::cout << "\nDSLK DON\n";
+    ds_don.print();
+
+    std::cout << "\nDSLK VONG\n";
+    ds_vong.print();
+
+    std::cout << "\nDSLK KEP\n";
+    ds_kep.print();
+
+    return 0;
 }
