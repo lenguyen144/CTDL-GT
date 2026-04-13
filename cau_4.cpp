7b224e1e10eff4e497a6e98af88c96607e9532af
lenguyen144 <lecongnguyen144@gmail.com>
2026-04-13 09:39:08 +0700

    thêm tên hàm cho ae dễ làm

---
+++ cau_4.cpp
@@ -0,0 +1,23 @@
+int p_x_de_quy(int n, int x) {
+
+}
+
+
+int p_x_y_de_quy(int n, int x, int y) {
+
+}
+
+
+int p_x_ko_de_quy(int n, int x) {
+
+}
+
+
+int p_x_y_ko_de_quy(int n, int x, int y) {
+
+}
+
+
+int main() {
+
+}
