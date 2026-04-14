454e47882ae50c27e8f706e5a96e3a0e64d1b0ad
lenguyen144 <lecongnguyen144@gmail.com>
2026-04-14 10:07:11 +0700

    hoàn thành p_x_y_de_quy

--- cau_4.cpp
+++ cau_4.cpp
@@ -1,23 +1,29 @@
-int p_x_de_quy(int n, int x) {
+#include <iostream>
 
+
+int p_x_de_quy(int n, int x) {
+	return 0;
 }
 
 
 int p_x_y_de_quy(int n, int x, int y) {
+	if (n <= 0) return 0;
+	if (n == 1) return 2*x + 3*y - 8;
 
+	return p_x_y_de_quy(n - 1, x, y) + p_x_y_de_quy(n - 2, x, y);
 }
 
 
 int p_x_ko_de_quy(int n, int x) {
-
+	return 0;
 }
 
 
 int p_x_y_ko_de_quy(int n, int x, int y) {
-
+	return 0;
 }
 
 
 int main() {
-
+	std::cout << "p_x_y_de_quy(n = 10, x = -5, y = 3): " << p_x_y_de_quy(2, -5, 3);
 }
