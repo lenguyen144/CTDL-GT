7a563ff765f84c31c99e8d344b950abbfa6ea225
lenguyen144 <lecongnguyen144@gmail.com>
2026-04-20 21:35:11 +0700

    cập nhật câu

--- cau_4.cpp
+++ cau_4.cpp
@@ -8,7 +8,7 @@ int p_x_de_quy(int n, int x) {
 
 
 int p_x_y_de_quy(int n, int x, int y) {
-	if (n <= 0) return 0;
+	if (n == 0) return 1;
 	if (n == 1) return 2*x + 3*y - 8;
 
 	return p_x_y_de_quy(n - 1, x, y) + p_x_y_de_quy(n - 2, x, y);
@@ -16,7 +16,19 @@ int p_x_y_de_quy(int n, int x, int y) {
 
 
 int p_x_ko_de_quy(int n, int x) {
-	return 0;
+	if (n == 0) return 1;
+	if (n == 1) return x;
+
+	int p0 = 1;
+	int p1 = x;
+	int pn = 0;
+	for (int i = 2; i <= n; i++) {
+		pn = p1 - p0;
+		p0 = p1;
+		p1 = pn;
+	}
+
+	return pn;
 }
 
 
@@ -40,6 +52,7 @@ int p_x_y_ko_de_quy(int n, int x, int y) {
 
 int main() {
 	std::cout<<"p_x_de_quy(n=5 , x=2): "<<p_x_de_quy(5,2)<<'\n';
+	std::cout << "p_x_ko_de_quy(n = 5, x = 2): " << p_x_ko_de_quy(5, 2) << "\n";
 	std::cout << "p_x_y_de_quy(n = 10, x = -5, y = 3): " << p_x_y_de_quy(2, -5, 3) <<std::endl;
 	std::cout << "p_x_y_ko_de_quy(n = 10, x = -5, y = 3): " << p_x_y_ko_de_quy(2, -5, 3) <<std::endl;
 }
